3eb9080714509cc56200ae19e89b4e9fb09cacb9
SaymonPenta <saymonlopesgeorg@gmail.com>
2022-10-24 09:24:13 -0400

    pageadmin e tpl admin

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ require_once("vendor/hcodebr/php-classes/src/DB/Sql.php");
 
 use\Slim\Slim;
 use Hcode\Page;
+use Hcode\PageAdmin;
 
 $app = new \Slim\Slim();
 
@@ -14,6 +15,14 @@ $app->config('debug', true);
 $app->get('/', function() {
 
 	$page = new Page();
+	
+	$page->setTpl("index");
+
+});
+
+$app->get('/admin', function() {
+
+	$page = new PageAdmin();
 	
 	$page->setTpl("index");
 
