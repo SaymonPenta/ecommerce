c087717cc22b35e1485d75cd3f91969f3b84b695
SaymonPenta <saymonlopesgeorg@gmail.com>
2022-10-25 10:42:26 -0400

    tela de login

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-
+session_start();
 require_once("vendor/autoload.php");
 
 require_once("vendor/hcodebr/php-classes/src/DB/Sql.php");
@@ -7,6 +7,7 @@ require_once("vendor/hcodebr/php-classes/src/DB/Sql.php");
 use\Slim\Slim;
 use Hcode\Page;
 use Hcode\PageAdmin;
+use\Hcode\Model\User;
 
 $app = new \Slim\Slim();
 
@@ -22,9 +23,40 @@ $app->get('/', function() {
 
 $app->get('/admin', function() {
 
+	User::verifyLogin();
+
 	$page = new PageAdmin();
 	
 	$page->setTpl("index");
+
+});
+
+$app->get('/admin/login', function(){
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("login");
+
+});
+
+$app->post('/admin/login', function(){
+
+	User::login($_POST["login"], $_POST["password"]);
+
+	header("Location: /admin");
+	exit;
+
+});
+
+$APP->GET('/admin/logout', function() {
+
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
 
 });
 
