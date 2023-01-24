e95fa2d5566dd9f33641a82a0f9b349778e10d02
SaymonPenta <saymonlopesgeorg@gmail.com>
2023-01-24 10:09:20 -0400

    Admin Pedidos

--- functions.php
+++ functions.php
@@ -8,7 +8,14 @@ function formatPrice($vlprice)
 
     if (!$vlprice > 0) $vlprice = 0;
 
-    return number_format($vlprice, 2, ",", " ");
+    return number_format($vlprice, 2, ",", ".");
+
+}
+
+function formatDate($date)
+{
+
+    return date ('d/m/Y', strtotime($date));
 
 }
 
--- vendor/hcodebr/php-classes/src/Model/Order.php
+++ vendor/hcodebr/php-classes/src/Model/Order.php
@@ -4,52 +4,146 @@ namespace Hcode\Model;
 
 use \Hcode\DB\Sql;
 use \Hcode\Model;
+use \Hcode\Model\Cart;
 
 class Order extends Model {
 
-    public function save()
-    {
+	const SUCCESS = "Order-Success";
+	const ERROR = "Order-Error";
 
-        $sql = new Sql();
+	public function save()
+	{
 
-        $results = $sql->select("CALL sp_orders_save(:idorder, :idcart, :iduser, :idstatus, :idaddress, :vltotal)", [
-            ':idorder'=>$this->getidorder(),
-            ':idcart'=>$this->getidcart(),
-            ':iduser'=>$this->getiduser(),
-            ':idstatus'=>$this->getidstatus(),
-            ':idaddress'=>$this->getidaddress(),
-            ':vltotal'=>$this->getvltotal()
-        ]);
+		$sql = new Sql();
 
-        if (count($results) > 0) {
-            $this->setData($results[0]);
-        }
+		$results = $sql->select("CALL sp_orders_save(:idorder, :idcart, :iduser, :idstatus, :idaddress, :vltotal)", [
+			':idorder'=>$this->getidorder(),
+			':idcart'=>$this->getidcart(),
+			':iduser'=>$this->getiduser(),
+			':idstatus'=>$this->getidstatus(),
+			':idaddress'=>$this->getidaddress(),
+			':vltotal'=>$this->getvltotal()
+		]);
 
-    }
+		if (count($results) > 0) {
+			$this->setData($results[0]);
+		}
 
-    public function get ($idorder)
-    {
+	}
 
-        $sql = new Sql();
+	public function get($idorder)
+	{
 
-        $results = $sql->select("
-            SELECT *
-            FROM tb_orders a 
-            INNER JOIN tb_orderstatus b USING(idstatus) 
-            INNER JOIN tb_carts c USING(idcart)
-            INNER JOIN tb_users d ON d.iduser = a.iduser
-            INNER JOIN tb_addresses e USING(idaddress)
-            INNER JOIN tb_persons f ON f.idperson = d.idperson
-            WHERE a.idorder = :idorder
-        ", [
-            ':idorder'=>$idorder
-        ]);
+		$sql = new Sql();
 
-        if (count($results) > 0) {
-            $this->setData($results[0]);
-        }
+		$results = $sql->select("
+			SELECT * 
+			FROM tb_orders a 
+			INNER JOIN tb_ordersstatus b USING(idstatus) 
+			INNER JOIN tb_carts c USING(idcart)
+			INNER JOIN tb_users d ON d.iduser = a.iduser
+			INNER JOIN tb_addresses e USING(idaddress)
+			INNER JOIN tb_persons f ON f.idperson = d.idperson
+			WHERE a.idorder = :idorder
+		", [
+			':idorder'=>$idorder
+		]);
 
-    }
+		if (count($results) > 0) {
+			$this->setData($results[0]);
+		}
+
+	}
+
+	public static function listAll()
+	{
+
+		$sql = new Sql();
+
+		return $sql->select("
+			SELECT * 
+			FROM tb_orders a 
+			INNER JOIN tb_ordersstatus b USING(idstatus) 
+			INNER JOIN tb_carts c USING(idcart)
+			INNER JOIN tb_users d ON d.iduser = a.iduser
+			INNER JOIN tb_addresses e USING(idaddress)
+			INNER JOIN tb_persons f ON f.idperson = d.idperson
+			ORDER BY a.dtregister DESC
+		");
+
+	}
+
+    public function delete()
+	{
+
+		$sql = new Sql();
+
+		$sql->query("DELETE FROM tb_orders WHERE idorder = :idorder", [
+			':idorder'=>$this->getidorder()
+		]);
+
+	}
+
+	public function getCart():Cart
+	{
+
+		$cart = new Cart();
+
+		$cart->get((int)$this->getidcart());
+
+		return $cart;
+
+	}
+
+	public static function setError($msg)
+	{
+
+		$_SESSION[Order::ERROR] = $msg;
+
+	}
+
+	public static function getError()
+	{
+
+		$msg = (isset($_SESSION[Order::ERROR]) && $_SESSION[Order::ERROR]) ? $_SESSION[Order::ERROR] : '';
+
+		Order::clearError();
+
+		return $msg;
+
+	}
+
+	public static function clearError()
+	{
+
+		$_SESSION[Order::ERROR] = NULL;
+
+	}
+
+	public static function setSuccess($msg)
+	{
+
+		$_SESSION[Order::SUCCESS] = $msg;
+
+	}
+
+	public static function getSuccess()
+	{
+
+		$msg = (isset($_SESSION[Order::SUCCESS]) && $_SESSION[Order::SUCCESS]) ? $_SESSION[Order::SUCCESS] : '';
+
+		Order::clearSuccess();
+
+		return $msg;
+
+	}
+
+	public static function clearSuccess()
+	{
+
+		$_SESSION[Order::SUCCESS] = NULL;
+
+	}
 
 }
 
